b4695f81f026381f834ddb8c4836b89411cebe7a
Madison Poore <madisonpoore@Madisons-MacBook-Pro.local>
2026-04-30 15:42:31 -0600

    final commits

--- Replication/Replication-Shrinking-the-Cross-Section/scs_main.py
+++ Replication/Replication-Shrinking-the-Cross-Section/scs_main.py
@@ -15,9 +15,9 @@ interactions = False
 rotate_PC = False
 withhold_test_sample = False
 #dataprovider = 'ff25'
-#dataprovider = 'anom_50'
+dataprovider = 'anom_50'
 #dataprovider = 'emerging_mkt'
-dataprovider = 'developed_mkt_exus'
+#dataprovider = 'developed_mkt_exus'
 #dataprovider = 'qqq'
 
 #use these dates for ff25 and anom50
